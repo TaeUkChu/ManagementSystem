4a2836c71e4ae266ca73e166134eb5bc9992636b
추태욱 <@DESKTOP-B43SNUM>
2020-03-24 14:59:27 +0900

    Product Manager

---
+++ src/productmanager.java
@@ -0,0 +1,11 @@
+public class productmanager {
+  public static void main(String[] args) {
+    System.out.println("*** Product inventory ***");
+    System.out.println("**Choose selection 1-5**");
+    System.out.println("1. View Product");
+    System.out.println("2. Add New Product");
+    System.out.println("3. Edit Product");
+    System.out.println("4. Delete Product");
+    System.out.println("5. End");
+    }
+}
